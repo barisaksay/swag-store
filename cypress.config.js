ea97ea57df88430e87db2387d1d6b18a20a20c31
baris_aksay <barish.aksay@gmail.com>
2023-11-29 16:58:09 +0100

    excludeSpecPattern added to config file to get rid of scaffold specs. Added explanation to AddToCart method.

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: "https://www.saucedemo.com/v1/",
-    watchForFileChanges: false
+    watchForFileChanges: false,
+    excludeSpecPattern: ['cypress/e2e/1-getting-started','cypress/e2e/2-advanced-examples']
   },
 });
--- cypress/e2e/POM/inventoryPOM.js
+++ cypress/e2e/POM/inventoryPOM.js
@@ -3,6 +3,10 @@ class inventoryPOM {
 
     addToCart(nthItem){
     // stores the item name in the alias "itemName", it can be used in test separately
+
+        //solution: below code selects the nth item from the item list,
+        //reads its name via invoke('text') and aliases it, so it can be used outside
+        //the POM method, ie in the test itself. For ex: see the --add to cart-- test.
     cy.get('.inventory_item_name')
     .eq(nthItem - 1)
     .invoke('text')
