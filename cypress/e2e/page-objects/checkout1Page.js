ab9df7451d66f2c5867e049dd636b47e8bc46f72
baris_aksay <barish.aksay@gmail.com>
2024-08-02 16:00:14 +0200

    added checkout1 helpers

--- cypress/e2e/page-objects/checkout1Page.js
+++ cypress/e2e/page-objects/checkout1Page.js
@@ -6,4 +6,29 @@ class Checkout1Page{
     postcodeField="input[data-test='postalCode']";
     continueButton="input[class='btn_primary cart_button']";
     cancelButton="a[class='cart_cancel_link btn_secondary']";
+    errorMessageCheckout1="h3[data-test='error']";
+
+
+    //helpers
+
+    typeFirstName(name){
+        cy.get(this.firstNameField).focus().type(name)
+        return this;
+    }
+
+    typeLastName(lastname){
+        cy.get(this.lastNameField).focus().type(lastname)
+        return this;
+    }
+
+    typePostcode(postcode){
+        cy.get(this.lastNameField).focus().type(postcode)
+        return this;
+    }
+
+    clickContinueButton(){
+        cy.get(this.continueButton).click()
+        return this;
+    }
+
 }
