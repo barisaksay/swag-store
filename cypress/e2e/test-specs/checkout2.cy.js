3154a37387234ae49668afad99418751e5e7175b
baris_aksay <barish.aksay@gmail.com>
2024-08-13 15:54:57 +0200

    added test:total amount shown correctly

--- cypress/e2e/test-specs/checkout2.cy.js
+++ cypress/e2e/test-specs/checkout2.cy.js
@@ -18,7 +18,31 @@ describe("checkout 2 page tests", () => {
     cy.login(validUsername, correctPassword);
   });
 
-  it("correct item price is shown at the final checkout",{tags:"@regression"}, () => {
+  it("correct item price is shown at the final checkout",{ tags: "@regression" },() => {
+      InventoryPage.addToCart(0);
+      cy.get("@itemPrice").then((itemPrice1) => {
+        itemPrice1 = parseFloat(itemPrice1.replace("$", ""));
+
+        InventoryPage.goToCart();
+        CartPage.clickCheckoutButton();
+        Checkout1Page.typeFirstName(firstname)
+          .typeLastName(lastname)
+          .typePostcode(postalcode)
+          .clickContinueButton();
+
+        cy.get(Checkout2Page.itemPriceLocator)
+          .invoke("text")
+          .then((itemPrice2) => {
+            itemPrice2 = itemPrice2.replace("$", " ");
+            itemPrice2 = parseFloat(itemPrice2);
+
+            cy.wrap(itemPrice2).should("eq", itemPrice1);
+          });
+      });
+    }
+  );
+
+  it.only("total amount to pay is calculated correctly",{ tags: "@regression" },()=>{
     InventoryPage.addToCart(0);
     cy.get("@itemPrice").then((itemPrice1) => {
       itemPrice1 = parseFloat(itemPrice1.replace("$", ""));
@@ -30,18 +54,24 @@ describe("checkout 2 page tests", () => {
         .typePostcode(postalcode)
         .clickContinueButton();
 
-      cy.get(Checkout2Page.itemPriceLocator)
-        .invoke("text")
+        cy.get('.summary_total_label')        .invoke("text")
         .then((itemPrice2) => {
-          itemPrice2 = itemPrice2.replace("$", " ");
+          itemPrice2 = itemPrice2.replace("Total: $", " ");
           itemPrice2 = parseFloat(itemPrice2);
 
-          cy.wrap(itemPrice2).should("eq", itemPrice1);
+          cy.wrap(itemPrice2).should("not.eq", itemPrice1);
+
+          let totalDue= itemPrice1+(itemPrice1*8/100)
+          
+          totalDue = parseFloat(totalDue.toFixed(2))
+
+          cy.wrap(itemPrice2).should("eq", totalDue);
+
         });
     });
-  });
+  })
 
-  it.skip("challenge: get every item name and store in aray", () => {
+  it("challenge: get every item name and store in aray", () => {
     let arrayNames = [];
     cy.get(".inventory_item")
       .each((item) => {
@@ -56,6 +86,30 @@ describe("checkout 2 page tests", () => {
       .then(() => {
         // log the final array outside the loop
         cy.log(arrayNames);
+      });
+  });
+
+  it("challenge: get every item name and price and store in an array ", () => {
+    let arrayNamesPrices = [];
+    cy.get(".inventory_item")
+      .each((item) => {
+        // Find the .inventory_item_name within each .inventory_item
+        cy.wrap(item)
+          .find(".inventory_item_name")
+          .invoke("text")
+          .then((itemName) => {
+            // Find the .inventory_item_price within the same .inventory_item
+            cy.wrap(item)
+              .find(".inventory_item_price")
+              .invoke("text")
+              .then((itemPrice) => {
+                // Push both name and price into the array
+                arrayNamesPrices.push({ name: itemName, price: itemPrice });
+              });
+          });
+      })
+      .then(() => {
+        cy.log(arrayNamesPrices);
       });
   });
 });
